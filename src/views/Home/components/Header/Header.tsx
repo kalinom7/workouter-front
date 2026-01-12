2b42282b93f3275aa7a3e81e190bc26c893204d9
Michał Kalinowski <michal@kalinowski.one>
2026-01-12 18:02:11 +0100

    feat: add test setup

--- src/views/Home/components/Header/Header.tsx
+++ src/views/Home/components/Header/Header.tsx
@@ -2,12 +2,10 @@ type HeaderProps = {
   readonly user: string
 }
 
-function Header( { user }: HeaderProps ) {
+export const Header = ({ user }: HeaderProps ) => {
   return (
     <header>
       <h1> Welcome, {user}!</h1>
     </header>
   )
-}
-
-export default Header
+} 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { playwright } from "@vitest/browser-playwright";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    browser: {
+      provider: playwright(),
+      headless: true,
+      enabled: true,
+      // at least one instance is required
+      instances: [{ browser: "chromium" }]
+    },
+  },
+});

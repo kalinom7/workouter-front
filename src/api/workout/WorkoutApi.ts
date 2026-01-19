6cb61cb43ed1f0dad62087516780471309e1e84a
kalinom7 <mateusz29122004@gmail.com>
2026-01-19 17:14:06 +0100

    fix: add mising types for WokoutApi

--- src/api/workout/WorkoutApi.ts
+++ src/api/workout/WorkoutApi.ts
@@ -1,85 +1,157 @@
 import { apiFetch } from "../fetch";
+import type { Workout } from "../../types/WorkoutTypes";
 
-export class WorkoutApi{
+export class WorkoutApi {
+  static async startWorkoutFromTemplate(
+    userId: string,
+    workoutTemplateId: string,
+  ) {
+    return apiFetch<Workout>(`/workouts/start/from-template?userId=${userId}`, {
+      method: "POST",
+      body: JSON.stringify({ workoutTemplateId }),
+    });
+  }
 
-    static async startWorkoutFromTemplate(userId: string, workoutTemplateId: string){
-        return apiFetch(`/workouts/start/from-template?userId=${userId}`, {
-            method: "POST",
-            body: JSON.stringify({ workoutTemplateId }),
-        });
-    }
-    
-    static async startEmptyWorkout(userId: string){
-        return apiFetch(`/workouts/start/empty?userId=${userId}`, {
-            method: "POST",
-        });
-    }
+  static async startEmptyWorkout(userId: string) {
+    return apiFetch<Workout>(`/workouts/start/empty?userId=${userId}`, {
+      method: "POST",
+    });
+  }
 
-    static async getWorkout(userId: string, workoutId: string){
-        return apiFetch(`/workouts/${workoutId}?userId=${userId}`, {
-            method: "GET",
-        });
-    }
+  static async getWorkout(userId: string, workoutId: string) {
+    return apiFetch<Workout>(`/workouts/${workoutId}?userId=${userId}`, {
+      method: "GET",
+    });
+  }
 
-    static async finishWorkout(userId: string, workoutId: string){
-        return apiFetch(`/workouts/${workoutId}/finish?userId=${userId}`, {
-            method: "PATCH",
-        });
-    }
+  static async finishWorkout(userId: string, workoutId: string) {
+    return apiFetch<Workout>(`/workouts/${workoutId}/finish?userId=${userId}`, {
+      method: "PATCH",
+    });
+  }
 
-    static async addExerciseToWorkout(userId: string, workoutId: string, exerciseId: string){
-        return apiFetch(`/workouts/${workoutId}/exercises?userId=${userId}`, {
-            method: "POST",
-            body: JSON.stringify({ exerciseId }),
-        });
-    }
+  static async addExerciseToWorkout(
+    userId: string,
+    workoutId: string,
+    exerciseId: string,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises?userId=${userId}`,
+      {
+        method: "POST",
+        body: JSON.stringify({ exerciseId }),
+      },
+    );
+  }
 
-    static async removeExerciseFromWorkout(userId: string, workoutId: string, exerciseOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}?userId=${userId}`, {
-            method: "DELETE",
-        });
-    }
+  static async removeExerciseFromWorkout(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}?userId=${userId}`,
+      {
+        method: "DELETE",
+      },
+    );
+  }
 
-    static async addSet(userId: string, workoutId: string, exerciseOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/sets?userId=${userId}`, {
-            method: "POST",
-        });
-    }
+  static async addSet(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/sets?userId=${userId}`,
+      {
+        method: "POST",
+      },
+    );
+  }
 
-    static async removeSet(userId: string, workoutId: string, exerciseOrder: number, setOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}?userId=${userId}`, {
-            method: "DELETE",
-        });
-    }
+  static async removeSet(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+    setOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}?userId=${userId}`,
+      {
+        method: "DELETE",
+      },
+    );
+  }
 
-    static async addWeightAndRepsToSet(userId: string, workoutId: string, exerciseOrder: number, setOrder:number, weight: number, reps: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}?userId=${userId}`, {
-            method: "PATCH",
-            body: JSON.stringify({ weight, reps }),
-        });
-    }
+  static async addWeightAndRepsToSet(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+    setOrder: number,
+    weight: number,
+    reps: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}?userId=${userId}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({ weight, reps }),
+      },
+    );
+  }
 
-    static async markSetAsCompleted(userId: string, workoutId: string, exerciseOrder: number, setOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}/complete?userId=${userId}`, {
-            method: "PATCH",
-        });
-    }
+  static async markSetAsCompleted(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+    setOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}/complete?userId=${userId}`,
+      {
+        method: "PATCH",
+      },
+    );
+  }
 
-    static async markSetAsUncompleted(userId: string, workoutId: string, exerciseOrder: number, setOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}/uncomplete?userId=${userId}`, {
-            method: "PATCH",
-        });
-    }
+  static async markSetAsUncompleted(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+    setOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/sets/${setOrder}/uncomplete?userId=${userId}`,
+      {
+        method: "PATCH",
+      },
+    );
+  }
 
-    static async markExerciseAsCompleted(userId: string, workoutId: string, exerciseOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/complete?userId=${userId}`, {
-            method: "PATCH",
-        });
-    }
+  static async markExerciseAsCompleted(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/complete?userId=${userId}`,
+      {
+        method: "PATCH",
+      },
+    );
+  }
 
-    static async markExerciseAsUncompleted(userId: string, workoutId: string, exerciseOrder: number){
-        return apiFetch(`/workouts/${workoutId}/exercises/${exerciseOrder}/uncomplete?userId=${userId}`, {
-            method: "PATCH",
-        });
-    }
+  static async markExerciseAsUncompleted(
+    userId: string,
+    workoutId: string,
+    exerciseOrder: number,
+  ) {
+    return apiFetch<Workout>(
+      `/workouts/${workoutId}/exercises/${exerciseOrder}/uncomplete?userId=${userId}`,
+      {
+        method: "PATCH",
+      },
+    );
+  }
 }

f6ab8647bc6c85fbb37141fc21d6f895be49cb61
kalinom7 <mateusz29122004@gmail.com>
2026-03-05 16:40:53 +0100

    refactor: divide WorkoutTemplateEditExercise view into view and editor component

--- src/views/WorkoutTemplate/EditExercise/WorkoutTemplateEditExerciseView.tsx
+++ src/views/WorkoutTemplate/EditExercise/WorkoutTemplateEditExerciseView.tsx
@@ -1,77 +1,35 @@
-import { useEditWorkoutTemplateExercise } from "@/api/workouttemplate/useEditWorkoutTemplateExercise";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { WorkoutTemplateContext } from "@/routes/workoutTemplate/WorkoutTemplateContext";
-import { useContext, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { toast } from "sonner";
-import { useQueryClient } from "@tanstack/react-query";
+import { useContext } from "react";
+import { useGetWorkoutTemplate } from "@/api/workouttemplate/useGetWorkoutTemplate";
+import { WorkoutTemplateExerciseEditor } from "./WorkoutTemplateExerciseEditor";
+import { useParams } from "react-router-dom";
 
-const someUuid = "123e4567-e89b-12d3-a456-426614174000";
+const someUuid = "123e4567-e89b-12d3-a456-426614174000"; // example of userId
 
 export const WorkoutTemplateEditExerciseView = () => {
-  const { mutate, isPending } = useEditWorkoutTemplateExercise();
   const { id } = useContext(WorkoutTemplateContext);
-
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const {
+    data,
+    isPending: isGetPending,
+    isError: isGetError,
+  } = useGetWorkoutTemplate(id, someUuid);
   const params = useParams();
-
-  const exerciseId = searchParams.get("exerciseId") ?? "";
   const order = Number(params.order);
-  const oldSets = Number(searchParams.get("sets"));
-  const oldRestPeriod = Number(searchParams.get("restPeriod"));
+  if (isGetPending) return <>Loading...</>;
+  if (isGetError || !data) return <>Error loading exercise</>;
 
-  const [newSets, setNewSets] = useState(oldSets);
-  const [newRestPeriod, setNewRestPeriod] = useState(oldRestPeriod);
-  const queryClient = useQueryClient();
+  const exercise = data.exercises.find(
+    (ex) => ex.order === Number(params.order),
+  );
 
-  const onSuccess = () => {
-    toast.success("Exercise successfully edited!");
-    queryClient.invalidateQueries({
-      queryKey: ["workout-templates", id, someUuid],
-    });
-    navigate(`/workout-template/${id}/exercises`);
-  };
-
-  const onError = () => {
-    toast.error("Failed to edit the exercise in workout template.");
-  };
-  const onEditExerciseClick = () => {
-    mutate(
-      {
-        userId: someUuid,
-        exerciseId,
-        workoutTemplateId: id,
-        order,
-        newSets,
-        newRestPeriod,
-      },
-      { onSuccess, onError },
-    );
-  };
-  const onSelectExerciseClick = () => {
-    navigate(`/workout-template/${id}/exercise/${order}/select?mode=edit`);
-  };
+  if (!exercise) return <>Exercise not found</>;
 
   return (
-    <>
-      <h1>Edit Exercise</h1>
-      <Button onClick={onSelectExerciseClick}>{exerciseId}</Button>
-
-      <Input
-        type="number"
-        value={newSets}
-        onChange={(e) => setNewSets(Number(e.target.value))}
-      />
-      <Input
-        type="number"
-        value={newRestPeriod}
-        onChange={(e) => setNewRestPeriod(Number(e.target.value))}
-      />
-      <Button disabled={isPending} onClick={onEditExerciseClick}>
-        Edit Exercise
-      </Button>
-    </>
+    <WorkoutTemplateExerciseEditor
+      oldSets={exercise.sets}
+      oldRestPeriod={exercise.restPeriod}
+      templateId={id}
+      order={order}
+    />
   );
 };
---
+++ src/views/WorkoutTemplate/EditExercise/WorkoutTemplateExerciseEditor.tsx
@@ -0,0 +1,79 @@
+import { useEditWorkoutTemplateExercise } from "@/api/workouttemplate/useEditWorkoutTemplateExercise";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
+import { toast } from "sonner";
+
+const someUuid = "123e4567-e89b-12d3-a456-426614174000"; // example of userId
+
+export const WorkoutTemplateExerciseEditor = ({
+  oldSets,
+  oldRestPeriod,
+  templateId,
+  order,
+}: {
+  oldSets: number;
+  oldRestPeriod: number;
+  templateId: string;
+  order: number;
+}) => {
+  const [newSets, setNewSets] = useState(oldSets);
+  const [newRestPeriod, setNewRestPeriod] = useState(oldRestPeriod);
+  const [searchParams] = useSearchParams();
+  const exerciseId = searchParams.get("exerciseId") ?? "";
+  const navigate = useNavigate();
+  const onSelectExerciseClick = () => {
+    navigate(
+      `/workout-template/${templateId}/exercise/${order}/select?mode=edit`,
+    );
+  };
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useEditWorkoutTemplateExercise();
+  const onSuccess = () => {
+    toast.success("Exercise successfully edited!");
+    queryClient.invalidateQueries({
+      queryKey: ["workout-templates", templateId, someUuid],
+    });
+    navigate(`/workout-template/${templateId}/exercises`);
+  };
+
+  const onError = () => {
+    toast.error("Failed to edit the exercise in workout template.");
+  };
+  const onEditExerciseClick = () => {
+    mutate(
+      {
+        userId: someUuid,
+        exerciseId,
+        workoutTemplateId: templateId,
+        order,
+        newSets,
+        newRestPeriod,
+      },
+      { onSuccess, onError },
+    );
+  };
+  return (
+    <>
+      <h1>Edit Exercise</h1>
+      <Button onClick={onSelectExerciseClick}>{exerciseId}</Button>
+
+      <Input
+        type="number"
+        value={newSets}
+        onChange={(e) => setNewSets(Number(e.target.value))}
+      />
+      <Input
+        type="number"
+        value={newRestPeriod}
+        onChange={(e) => setNewRestPeriod(Number(e.target.value))}
+      />
+      <Button disabled={isPending} onClick={onEditExerciseClick}>
+        Edit Exercise
+      </Button>
+    </>
+  );
+};

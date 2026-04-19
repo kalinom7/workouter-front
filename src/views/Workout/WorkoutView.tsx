4fc324000eecde129e5c479295e4dbfb2b33814a
kalinom7 <mateusz29122004@gmail.com>
2026-04-19 21:26:22 +0200

    feat: add restPeriod timer after saving set

--- src/views/Workout/WorkoutView.tsx
+++ src/views/Workout/WorkoutView.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FinishWorkoutButton } from "./components/FinishWorkoutButton";
 import { WorkoutExerciseManager } from "./components/WorkoutExerciseManager";
 import { WorkoutTimer } from "./components/WorkoutTimer";
 import { WorkoutSummary } from "./components/WorkoutSummary";
+import { RestTimerContext } from "@/contexts/workout/RestTimerContext";
+import { RestTimer } from "./components/RestTimer";
 
 export const WorkoutView = () => {
   const [isWorkoutFinished, setIsWorkoutFinished] = useState(false);
+  const { restTimer } = useContext(RestTimerContext);
 
   return (
     <>
@@ -15,6 +18,7 @@ export const WorkoutView = () => {
       />
       <WorkoutExerciseManager />
       {isWorkoutFinished && <WorkoutSummary />}
+      {restTimer.isActive && <RestTimer />}
     </>
   );
 };

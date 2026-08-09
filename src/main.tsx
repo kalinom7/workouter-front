a399e64828b0128de21f49b7d81bcad772efb33b
kalinom7 <mateusz29122004@gmail.com>
2026-08-09 23:08:41 +0200

    feat: add Alexandria font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "@fontsource/alexandria/latin-100.css";
+import "@fontsource/alexandria/latin-200.css";
+import "@fontsource/alexandria/latin-300.css";
+import "@fontsource/alexandria/latin-400.css";
+import "@fontsource/alexandria/latin-600.css";
+import "@fontsource/alexandria/latin-700.css";
+import "@fontsource/alexandria/latin-800.css";
+import "@fontsource/alexandria/latin-900.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Alexandria", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};

dbba7a9e8dbb5666b13a8f57d7d0661e436c1f5b
kalinom7 <mateusz29122004@gmail.com>
2026-08-09 20:42:02 +0200

    refactor: improve register view style

--- src/views/Register/RegisterView.tsx
+++ src/views/Register/RegisterView.tsx
@@ -1,31 +1,28 @@
 import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
 import { RegisterForm } from "./components/RegisterForm";
+import { useNavigate } from "react-router-dom";
 
 export const RegisterView = () => {
   const navigate = useNavigate();
-
   return (
-    <>
+    <div className="h-screen flex flex-col justify-center items-center pb-5 ">
+      <main className="flex-1 flex flex-col justify-center w-full gap-12">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold">WORKOUTER</h1>
+          <p>LOGO</p>
+        </div>
+        <div className="w-full flex flex-col items-center gap-4">
+          <h2 className="text-2xl">Create new account</h2>
+          <RegisterForm />
+        </div>
+      </main>
       <Button
-        onClick={() => {
-          navigate(-1);
-        }}
+        variant="outline"
+        className="rounded-full w-55 text-base border-teal-300"
+        onClick={() => navigate("/")}
       >
-        Back
+        I already have an account
       </Button>
-      <h1>WORKOUTER</h1>
-      <p>LOGO</p>
-      <h2>Create new account</h2>
-      <RegisterForm />
-      <Button
-        onClick={() => {
-          navigate("/home");
-        }}
-      >
-        {" "}
-        Register{" "}
-      </Button>
-    </>
+    </div>
   );
 };
--- src/views/Register/components/RegisterForm.tsx
+++ src/views/Register/components/RegisterForm.tsx
@@ -1,15 +1,50 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const RegisterForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    return (
-        <>
-        <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Input placeholder="Confirm password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-        </>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onRegisterClick = () => {
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+    navigate("/home");
+  };
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <main className="flex flex-col gap-2 items-center">
+        <Input
+          className="w-75 rounded-full"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          className="w-75 rounded-full"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          className="w-75 rounded-full"
+          placeholder="Confirm password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </main>
+      <Button
+        onClick={onRegisterClick}
+        size="lg"
+        className="w-55 text-xl rounded-full bg-teal-300"
+      >
+        {" "}
+        Register{" "}
+      </Button>
+    </div>
+  );
+};

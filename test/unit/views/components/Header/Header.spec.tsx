cc02bb7fd6edb2796ec06b26945497dcba46765e
kalinom7 <mateusz29122004@gmail.com>
2026-08-10 00:14:34 +0200

    feat: init CI pipeline

--- test/unit/views/components/Header/Header.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-react'
-import { Header } from '../../../../../src/views/Home/components/Header/Header'
-
-test('renders Welcome message with user name', async () => {
-    const screen = await render(<Header user="John Doe" />)
-    await expect.element(screen.getByText('Welcome, John Doe')).toBeVisible()
-})

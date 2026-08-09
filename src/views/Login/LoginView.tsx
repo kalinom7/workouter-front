a79fecadaa71a24db31f57c89f03f24fbfa6c208
kalinom7 <mateusz29122004@gmail.com>
2026-08-09 20:24:10 +0200

    refactor: improve login view style

--- src/views/Login/LoginView.tsx
+++ src/views/Login/LoginView.tsx
@@ -9,13 +9,25 @@ export const LoginView = () => {
     navigate("/register");
   };
   return (
-    <>
-      <h1>WORKOUTER</h1>
-      <p>LOGO</p>
-      <h2>Welcome</h2>
-      <LoginForm />
-      <Button>Forgotten passowrd?</Button>
-      <Button onClick={onRegisterClick}>Create new account</Button>
-    </>
+    <div className="h-screen flex flex-col justify-center items-center pb-5 ">
+      <main className="flex-1 flex flex-col justify-center w-full gap-12">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold">WORKOUTER</h1>
+          <p>LOGO</p>
+        </div>
+        <div className="w-full flex flex-col items-center gap-4">
+          <h2 className="text-2xl ">Welcome!</h2>
+          <LoginForm />
+          <Button variant="ghost">Forgotten password?</Button>
+        </div>
+      </main>
+      <Button
+        variant="outline"
+        className="rounded-full w-55 text-base border-teal-300"
+        onClick={onRegisterClick}
+      >
+        Create new account
+      </Button>
+    </div>
   );
 };
--- src/views/Login/components/LoginForm.tsx
+++ src/views/Login/components/LoginForm.tsx
@@ -1,18 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { useState } from "react"
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    return (
-        <>
-        <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Button onClick={() => { navigate("/home") }}>Login</Button>
-        
-        </>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <main className="flex flex-col gap-2 items-center">
+        <Input
+          className="w-75 rounded-full"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          className="w-75 rounded-full"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </main>
+      <Button
+        size="lg"
+        className="w-55 text-xl rounded-full bg-teal-300"
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Login
+      </Button>
+    </div>
+  );
+};

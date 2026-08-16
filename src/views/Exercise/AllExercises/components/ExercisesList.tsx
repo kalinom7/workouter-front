db22203e66bcbd39c7d3147f6409b604cffa1938
kalinom7 <mateusz29122004@gmail.com>
2026-08-16 15:08:55 +0200

    add another e2e exercise test

--- src/views/Exercise/AllExercises/components/ExercisesList.tsx
+++ src/views/Exercise/AllExercises/components/ExercisesList.tsx
@@ -2,6 +2,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import type { Exercise } from "@/types/ExerciseTypes";
 import { useNavigate } from "react-router-dom";
 import { ExerciseCard } from "./ExerciseCard";
+import { globalUserId } from "@/utils/globalUserId";
 
 export const ExercisesList = ({
   exercises,
@@ -16,7 +17,7 @@ export const ExercisesList = ({
   );
 
   const onExerciseCardClick = (id: string) => {
-    navigate(`${id}`);
+    navigate(`${id}?userId=${globalUserId}`);
   };
 
   return (
